dfb1d4587edefdb424accd39f04a2c1e8785b667
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-07-23 17:30:51 +0200

    feat: modifica tabella projects per aggiunta chiave sterna

---
+++ database/migrations/2024_07_23_145100_add_type_id_foreign_to_projects_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+
+            //Creo la colonna
+            $table->unsignedBigInteger('type_id')->nullable()->after('id');
+
+            //Dico a laravel che è un a chiave esterna
+            $table->foreign('type_id')
+            ->references('id')
+            ->on('types')
+            // Quando viene eliminato un tipo in caso sia relazionato a uno o più progetti, il campo type_id risulterà NULL
+            ->nullOnDelete();
+
+            // Zucchero sintattico (versione abbreviata)
+            // $table->foreignId('type_id')->constrained
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+
+            //scollego la chiave
+            $table->dropForeign('projects_type_id_foreign');
+
+            // droppo la colonna
+            $table->dropColumn('type_id');
+        });
+    }
+};
